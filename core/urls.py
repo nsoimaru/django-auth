712d8abe436a0b5e1f4a00664056b824dcf66d87
Nelu Soimaru <nsoimaru@gmail.com>
2022-05-22 18:26:22 +0300

    added registration and login api

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 
-from .views import RegisterAPIView
+from .views import RegisterAPIView, LoginAPIView
 
 urlpatterns = [
     path('register', RegisterAPIView.as_view(), name='register'),
+    path('login', LoginAPIView.as_view(), name='login'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
+import email
+from urllib import response
 from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from core.authentication import create_access_token
+
+from core.models import User
 
 from .serializer import UserSerializer
 
@@ -17,3 +22,26 @@ class RegisterAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data)
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('Invalid credentials!')
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Invalid credentials!')
+
+        access_token = create_access_token(user.id)
+        refresh_token = create_access_token(user.id)
+
+        response = Response()
+        response.set_cookie(key='refresh_token', value=refresh_token, httponly=True)
+        response.data = {
+            'token': access_token
+        }
+        return response
